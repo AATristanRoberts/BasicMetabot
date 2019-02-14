0f1d11325f555a498c62546b19e7baf8981f1c7a
Tristan Roberts <tristan.roberts@automationanywhere.com>
2019-02-14 12:47:14 +1100

    initial

---
+++ MetabotExample/MetabotExample/MetabotExampleCls.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetabotExample
+{
+    public class MetabotExampleCls
+    {
+        public string HelloName(string inputName)
+        {
+            return "Hello " + inputName;
+        }
+
+        public int addTwo(int n)
+        {
+            return 2 + n;
+        }
+    }
+}
